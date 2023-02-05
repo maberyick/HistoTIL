45ffdf0ef6bfc9168abc2db0a11b831dc8b41b19
Cristian Barrera <maberyick@gmail.com>
2023-02-05 16:07:36 -0500

    updates on the use of the segmentation to loop through fodlers

--- epistroma.py
+++ epistroma.py
@@ -24,30 +24,62 @@ def main(args):
         print(' Best auc_sum: {:.3}'.format(best_auc_sum))
         print('=============================================')    
         print(' [*] Load Success!\n')
-    names = utils.all_files_under(args.input_path,'.png')
-    for name in names:
-        print(name)
-        savingName = args.output_path + os.path.basename(name)[:-4]
-        if os.path.isfile(savingName) or os.path.getsize(name)>>20 < 2:
-            print('size less than 2MB or file existed , continue to next')
-            continue
-        image = Image.open(name)
-        print(np.shape(image))
-        image = image.resize((2000,2000), Image.ANTIALIAS) 
-        print('predicting on image ', name)
-        image = np.expand_dims(image, axis=0)
-        print(np.shape(image))
-        samples = sess.run(tf.get_default_graph().get_tensor_by_name('g_/Sigmoid:0'), \
-                feed_dict={tf.get_default_graph().get_tensor_by_name('image:0'): image})
-        samples = np.squeeze(samples*255).astype(np.uint8)   
-        # save as png file
-        image = Image.fromarray(samples)
-        image.save(savingName+'.png', format='PNG')
+    if args.cohort_type == 'patch':
+        names = utils.all_files_under(args.input_path,'.png')
+        for name in names:
+            print(name)
+            savingName = args.output_path + os.path.basename(name)[:-4]
+            if os.path.isfile(savingName):
+                print('file existed, continue to next')
+                continue
+            image = Image.open(name)
+            print(np.shape(image))
+            image = image.resize((2000,2000), Image.ANTIALIAS) 
+            print('predicting on image ', name)
+            image = np.expand_dims(image, axis=0)
+            print(np.shape(image))
+            samples = sess.run(tf.get_default_graph().get_tensor_by_name('g_/Sigmoid:0'), \
+                    feed_dict={tf.get_default_graph().get_tensor_by_name('image:0'): image})
+            samples = np.squeeze(samples*255).astype(np.uint8)   
+            # save as png file
+            image = Image.fromarray(samples)
+            image.save(savingName+'.png', format='PNG')
+    elif args.cohort_type == 'folder':
+        # list of the folders (cohorts)
+        folder_list = utils.all_files_under(args.input_wsi)
+        for folder in folder_list:
+            folder_dir = os.path.splitext(folder)[0]
+            folder_name = os.path.splitext(os.path.basename(folder_dir))[0]
+            if not os.path.isdir(args.output_path + folder_name):
+                os.mkdir(args.output_path + folder_name)
+            # loop through the files of each folder
+            names = utils.all_files_under(args.input_path+folder_name,'.png')
+            for name in names:
+                print(name)
+                savingName = args.output_path + folder_name +'/'+ os.path.basename(name)[:-4]+'.png'
+                if os.path.isfile(savingName):
+                    print('file existed, continue to next')
+                    continue
+                image = Image.open(name)
+                print(np.shape(image))
+                image = image.resize((2000,2000), Image.ANTIALIAS) 
+                print('predicting on image ', name)
+                image = np.expand_dims(image, axis=0)
+                print(np.shape(image))
+                samples = sess.run(tf.get_default_graph().get_tensor_by_name('g_/Sigmoid:0'), \
+                        feed_dict={tf.get_default_graph().get_tensor_by_name('image:0'): image})
+                samples = np.squeeze(samples*255).astype(np.uint8)   
+                # save as png file
+                image = Image.fromarray(samples)
+                image.save(savingName, format='PNG')       
 
+    
 ### Working paths
 parser = argparse.ArgumentParser(description='configuration for running the Epithelium-Stroma segmentation')
-parser.add_argument('--input_path', type=str, default='./testimage/patch/', help='input directory')
+parser.add_argument('--input_wsi', type=str, default='./testimage/wsi/', help='input directory of the wsi (folder option)')
+parser.add_argument('--input_path', type=str, default='./testimage/patch/', help='input directory of the images (patch option)')
 parser.add_argument('--output_path', type=str, default='./testimage/epimask/', help='output directory for epistroma mask')
+parser.add_argument('--cohort_type', type=str, default='patch', help='Choose between patch or folder. running on a single folder (/cohort/*.png) of png images or a directory of folders (e.g. /cohorts/cohort1/*.png')
 
 args = parser.parse_args()
 
