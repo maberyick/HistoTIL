1ff4e7e88aa41ea80c46d504f3534db655aa07fa
Cristian Barrera <cbarr23@emory.edu>
2023-07-09 23:22:51 -0400

    Update gather_svs_name.py

--- gather_svs_name.py
+++ gather_svs_name.py
@@ -6,14 +6,14 @@ def get_svs_files(root_dir):
     count = 1
     file_list = []
     for root, dirs, files in os.walk(root_dir):
-        if 'Screening' in dirs:
-            screening_dir = os.path.join(root, 'Screening')
-            os.chdir(screening_dir)
+        if 'wsi' in dirs:
+            wsi_dir = os.path.join(root, 'wsi')
+            os.chdir(wsi_dir)
             for file in glob.glob("*.svs"):
                 if file.endswith('.svs'):
                     print(count)
                     count = count+1
-                    file_path = os.path.join(screening_dir, file)
+                    file_path = os.path.join(wsi_dir, file)
                     file_list.append((file_path))
     return file_list
 
@@ -24,8 +24,8 @@ def save_to_tsv(file_list, output_file):
     print(f"File list saved to '{output_file}'.")
 
 # Example usage
-root_directory = r'Z:\Raptomics\CA209-227'  # Replace with the desired root directory
-output_filename = r'C:\Users\barrerc2\Documents\wsi_data\svs_files.tsv'
+root_directory = '/mnt/datas1/'  # Replace with the desired root directory
+output_filename = '/mnt/datas2/histotil/svs_files.tsv'
 
 svs_files = get_svs_files(root_directory)
 save_to_tsv(svs_files, output_filename)
