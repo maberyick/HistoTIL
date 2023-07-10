fe72efdc8f8d5f110920d66843420c571fdefca7
Cristian Barrera <cbarr23@emory.edu>
2023-07-09 23:35:36 -0400

    Update gather_svs_name.py

--- gather_svs_name.py
+++ gather_svs_name.py
@@ -2,19 +2,19 @@ import os
 import csv
 import glob
 
-def get_svs_files(root_dir):
+def get_files_with_extensions(root_dir, extensions):
     count = 1
     file_list = []
     for root, dirs, files in os.walk(root_dir):
         if 'wsi' in dirs:
             wsi_dir = os.path.join(root, 'wsi')
             os.chdir(wsi_dir)
-            for file in glob.glob("*.svs"):
-                if file.endswith('.svs'):
+            for ext in extensions:
+                for file in glob.glob(f"*.{ext}"):
                     print(count)
-                    count = count+1
+                    count += 1
                     file_path = os.path.join(wsi_dir, file)
-                    file_list.append((file_path))
+                    file_list.append(file_path)
     return file_list
 
 def save_to_tsv(file_list, output_file):
@@ -26,6 +26,7 @@ def save_to_tsv(file_list, output_file):
 # Example usage
 root_directory = '/mnt/datas1/'  # Replace with the desired root directory
 output_filename = '/mnt/datas2/histotil/svs_files.tsv'
+extensions = ['svs', 'tif', 'ti']  # Add or remove extensions as needed
 
-svs_files = get_svs_files(root_directory)
-save_to_tsv(svs_files, output_filename)
+files_with_extensions = get_files_with_extensions(root_directory, extensions)
+save_to_tsv(files_with_extensions, output_filename)
