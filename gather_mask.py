585ca4222a75d80045c617af4f96f5a83564a89d
Cristian Barrera <cbarr23@emory.edu>
2023-07-21 10:47:36 -0400

    Update gather_mask.py

--- gather_mask.py
+++ gather_mask.py
@@ -3,17 +3,18 @@ import shutil
 import csv
 import argparse
 
-def find_and_copy_files(source_folder, destination_folder):
+def find_and_copy_files(source_folder, destination_folder, extensions):
     copied_files = []
     for root, dirs, files in os.walk(source_folder):
-        for file in files:
-            if file.endswith('.svs_mask_use.png'):
-                source_path = os.path.join(root, file)
-                new_file_name = file.replace('.svs_mask_use.png', '.png')
-                destination_path = os.path.join(destination_folder, new_file_name)
-                shutil.copy2(source_path, destination_path)
-                copied_files.append(new_file_name)
-                print(f"Copied {file} to {destination_path}")
+        for ext in extensions:
+            for file in files:
+                if file.endswith(f'.{ext}_mask_use.png'):
+                    source_path = os.path.join(root, file)
+                    new_file_name = file.replace(f'.{ext}_mask_use.png', f'.{ext}')
+                    destination_path = os.path.join(destination_folder, new_file_name)
+                    shutil.copy2(source_path, destination_path)
+                    copied_files.append(new_file_name)
+                    print(f"Copied {file} to {destination_path}")
 
     return copied_files
 
@@ -29,11 +30,14 @@ if __name__ == "__main__":
     parser.add_argument("--source_folder", type=str, help="Source folder path")
     parser.add_argument("--destination_folder", type=str, help="Destination folder path")
     parser.add_argument("--csv_file", type=str, help="CSV file path")
+    parser.add_argument("--extensions", nargs='+', default=['svs', 'tif', 'tiff'],
+                        help="List of file extensions to check for (space-separated). Default: svs tif tiff")
     args = parser.parse_args()
 
     source_folder = args.source_folder
     destination_folder = args.destination_folder
     csv_file = args.csv_file
+    extensions = args.extensions
 
-    copied_files = find_and_copy_files(source_folder, destination_folder)
+    copied_files = find_and_copy_files(source_folder, destination_folder, extensions)
     save_to_csv(copied_files, csv_file)
