69ab72ed3ee293e0aa514710b76af84351112e5e
Cristian Barrera <cbarr23@emory.edu>
2023-07-21 10:18:50 -0400

    Update gather_mask.py

--- gather_mask.py
+++ gather_mask.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import csv
+import argparse
 
 def find_and_copy_files(source_folder, destination_folder):
     copied_files = []
@@ -23,14 +24,16 @@ def save_to_csv(file_names, csv_file):
         for file_name in file_names:
             writer.writerow([file_name])
 
-# Example usage
-#source_folder = r'E:\Tools\histoqc\histoqc_output_20230614-141029'
-#destination_folder = r'E:\wsi_data\tissue_mask'
-#csv_file = r'E:\wsi_data\tissue_mask_filename_list.csv'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find and copy files and save to CSV.")
+    parser.add_argument("--source_folder", type=str, help="Source folder path")
+    parser.add_argument("--destination_folder", type=str, help="Destination folder path")
+    parser.add_argument("--csv_file", type=str, help="CSV file path")
+    args = parser.parse_args()
 
-source_folder = '/mnt/datas2/histotil/histoqc/yale/'
-destination_folder = '/mnt/datas2/histotil/glass_mask/yale/'
-csv_file = '/mnt/datas2/histotil/yale_svs_files.csv'
+    source_folder = args.source_folder
+    destination_folder = args.destination_folder
+    csv_file = args.csv_file
 
-copied_files = find_and_copy_files(source_folder, destination_folder)
-save_to_csv(copied_files, csv_file)
+    copied_files = find_and_copy_files(source_folder, destination_folder)
+    save_to_csv(copied_files, csv_file)
