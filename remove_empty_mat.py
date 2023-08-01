282b702f2aada4e1d8f9b377ce83cf8a8827a3c6
Cristian Barrera <cbarr23@emory.edu>
2023-08-01 19:06:21 -0400

    Update remove_empty_mat.py

--- remove_empty_mat.py
+++ remove_empty_mat.py
@@ -6,14 +6,14 @@ def delete_small_mat_files(directory, size_limit_kb):
         for file in files:
             if file.endswith(".mat"):
                 filepath = os.path.join(root, file)
-                if os.path.getsize(filepath) < size_limit_kb * 256:
+                if os.path.getsize(filepath) < size_limit_kb:
                     print(f"Deleting: {filepath}")
                     os.remove(filepath)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Delete small .mat files")
     parser.add_argument("path", type=str, help="Base directory path")
-    parser.add_argument("--size-limit", type=int, default=1, help="Size limit in Bytes (default: 1, size of 256 Bytes)")
+    parser.add_argument("--size-limit", type=int, default=1, help="Size limit in Bytes (default: 1, size of 1024 Bytes)")
 
     args = parser.parse_args()
     base_directory = args.path
